b68eb87c6ec6bdf1be06dd76a498dbf757a0a248
PrashantaPH <105696439+PrashantaPH@users.noreply.github.com>
2026-07-05 03:47:42 +0530

    Updated the axios alp call url

--- message-mesh-ui/src/api/axiosClient.ts
+++ message-mesh-ui/src/api/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken, useAuthStore } from '../store/authStore';
 
 export const axiosClient = axios.create({
-  baseURL: '/',
+  baseURL: 'https://message-mesh-dev.onrender.com/',
   headers: { 'Content-Type': 'application/json' },
 });
 
